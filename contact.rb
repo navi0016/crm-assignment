ddaeff52c3d8cdf9403cf6d7bbb2542a3205f517
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-17 16:41:12 -0400

    added class variables:contacts,next_id. created methods initializ,self.create, reader and writes for initalize

--- contact.rb
+++ contact.rb
@@ -1,19 +1,55 @@
 class Contact
 
+  @@contacts = []
+  @@next_id = 1
+
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize(first_name,last_name,email,note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+
+    @id = @@next_id
+    @@next_id += 1
 
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name,last_name,email,note)
+    new_contact = Contact.new(first_name,last_name,email,note)
+    @@contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
+    @@contacts
+  end
 
+  def id
+    @id
+  end
+
+  def email
+    @email
+  end
+
+  def note
+    @note
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   # This method should accept an id as an argument
@@ -22,7 +58,7 @@ class Contact
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -43,10 +79,6 @@ class Contact
 
   end
 
-  def full_name
-
-  end
-
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
@@ -54,5 +86,24 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+  def first_name=(update_first_name)
+    first_name = update_first_name
+  end
+
+  def last_name=(update_last_name)
+    last_name = update_last_name
+  end
+
+  def email=(update_email)
+    @email = update_email
+  end
+
+  def note=(update_note)
+    @note = update_note
+  end
 end
+
+# contact1 = Contact.new('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+# p contact.email
+# contact.note = "Loves HTMl & CSS"
+# p contact
